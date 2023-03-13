35e28ab28a4d68398a1ff55d9031e95872ede226
Bruno Kraychete da Costa <brunokc@kraychete.com>
2023-03-12 23:25:26 -0700

    Add comments to explain reasons and usage

--- src/datetimeutils.py
+++ src/datetimeutils.py
@@ -1,8 +1,14 @@
 from datetime import datetime, timezone
 from typing import Final
 
+# utcmin is an aware version of datetime.min
 utcmin: Final = datetime.min.replace(tzinfo=timezone.utc)
 
+# utcnow() is an aware (with timezone information) version of datetime.utcnow(),
+# which returns a naive datetime and had its use discouraged (we should alway
+# use aware datetimes when dealing with UTC) -- docs suggest the use of
+# datetime.now(timezone.utc) instead of datetime.utcnow(), which is what we use
+# here.
 def utcnow() -> datetime:
     return datetime.now(timezone.utc)
 
@@ -11,9 +17,9 @@ def to_iso_format(dt: datetime) -> str:
 
 def from_iso_format(dt: str) -> datetime:
     """Converts from a datetime in ISO 8601 format to a datetime object
-    Note that datetime.fromisoformat() doesn't fully support ISO 8601 in Python 3.9
-    (it seems Python 3.11 corrected this), so we have to drop the last "Z" from
-    the string as a workaround
+    Note that datetime.fromisoformat() doesn't fully support ISO 8601 in
+    Python 3.9 (it seems Python 3.11 corrected this), so we have to drop the
+    last "Z" from the string as a workaround.
     """
     if dt.endswith("Z"):
         return datetime.fromisoformat(dt[:-1])
